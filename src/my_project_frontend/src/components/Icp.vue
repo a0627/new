7296415836e2b4ab392591218216d73eeef849a4
a0627 godula <agata@godula.com>
2024-09-24 17:50:31 +0000

    Feat:Frontend

---
+++ src/my_project_frontend/src/components/Icp.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import { my_project_backend } from 'declarations/my_project_backend/index';
+let greeting = ref('');
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const target = e.target;
+  const name = target.querySelector('#name').value;
+  await my_project_backend.greet(name).then((response) => {
+    greeting.value = response;
+  });
+}
+</script>
+
+<template>
+  <main>
+    
+    <br />
+    <br />
+    <form action="#" @submit="handleSubmit">
+        <img src="/logo2.svg" alt="DFINITY logo"/>
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="text" />
+      <button type="submit">Click Me!</button>
+    </form>
+    <section id="greeting">{{ greeting }}</section>
+  </main>
+</template>
---
+++ src/my_project_frontend/src/components/aboutYou.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="about">
+        <h2>About You</h2>
+        <div class="about-content">
+            <p>{{ aboutText }}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "AboutUs",
+    data() {
+        return {
+            aboutText: "SHort description about you :)"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.about {
+    padding: 35px 20px;
+    background-color: #1c1c1a;
+    color: #ffffffff;
+    max-width: 1200px;
+    width:  100%;
+    border-radius: 8px;
+}
+
+.about h2 {
+    margin-left: 10px;
+}
+
+.about-content {
+    margin: 10px;
+    padding: 20px;
+    background: #ffffffff;
+    border: 1px solid #A3CFFF;
+    border-radius: 8px;
+}
+</style>
